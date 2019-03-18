70202f6aae1631e497b28231e5362dd586cc0b76
Anastasia Pelevina <pelevina.2000@mail.ru>
2019-03-18 19:16:59 +0500

    Initial commit

---
+++ GUI/gui.py
@@ -0,0 +1,38 @@
+import tkinter
+from PIL import ImageTk, Image
+from game import field
+
+
+class Application:
+    @staticmethod
+    def run():
+        Game.create_window()
+
+
+class Game:
+    @staticmethod
+    def generate_field(root, play_field):
+        image = ImageTk.PhotoImage(Image.open("images/blue_cell.png"))
+        bot_field_gui = tkinter.Frame(root, bd=5, bg='blue')
+        for x in range(len(play_field)):
+            for y in range(len(play_field[x])):
+                frame = tkinter.Frame(bot_field_gui)
+                cell = tkinter.Button(frame, bd=5, width=10,
+                                      height=10,
+                                      image=image)
+                cell.pack()
+                frame.grid(row=x, column=y)
+        bot_field_gui.pack()
+
+    @staticmethod
+    def create_window():
+        window = tkinter.Tk()
+        window.title('Battleship')
+        bot_field = field.Field.create_field()
+        Game.generate_field(window, bot_field)
+
+        window.mainloop()
+
+
+def process_click():
+    pass
---
+++ battleship.py
@@ -0,0 +1,6 @@
+#! /bin/local/python3
+from GUI import gui
+
+
+if __name__ == '__main__':
+    gui.Application.run()
